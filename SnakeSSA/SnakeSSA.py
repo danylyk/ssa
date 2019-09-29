60caa2b249f5a3e0826a5013565576fbfc8bdf78
danylyk <web.vitaly@rambler.ru>
2019-09-29 03:12:06 +0300

    App.class V1

--- SnakeSSA/SnakeSSA.py
+++ SnakeSSA/SnakeSSA.py
@@ -1,1 +1,28 @@
 import pygame
+
+class App:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Snake SSA")
+        self.window = pygame.display.set_mode((900, 480))
+        self.run = True
+        self.fps = 17 # 1000/60 - 60 fps
+        self.__Run()
+
+    def __Run(self):
+        while self.run:
+            pygame.time.delay(self.fps)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.run = False
+
+            self.__Draw()
+            pygame.display.update()
+        
+        pygame.quit()
+
+    def __Draw (self):
+        pygame.draw.rect(self.window, (255,0,0), (50, 50, 50, 50))
+
+
+app = App()
